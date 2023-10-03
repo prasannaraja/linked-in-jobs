0b17ac94012ec54757702baad685e55453c9a626
Prasanna Raja <prasannaraja@msn.com>
2023-10-03 11:31:27 +0200

    moved

--- src/service-worker.ts
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.someData) {
-    console.log(`Received data: ${message.someData}`);
-    // You can do something with the data here
-  }
-});
